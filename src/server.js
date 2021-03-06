d7ab4ee1fa4a345919aa07c453df14833a724dca
Connor Slade <turtleslade@gmail.com>
2021-03-06 00:32:35 -0500

    RateLimit and Ip

--- src/server.js
+++ src/server.js
@@ -7,29 +7,39 @@ const app = express();
 const dir = path.join(__dirname, config.dogPath);
 const dogs = fs.readdirSync(dir);
 
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+    windowMs: 30000,
+    max: 100,
+});
+
+app.use(limiter);
+
 app.get('/random', function (req, res) {
-    res.sendFile(`${dir}/${getRendomDog(dogs)}`)
+    res.sendFile(`${dir}/${getRendomDog(dogs)[1]}`)
 });
 
 app.get('/dog/:index', function (req, res) {
     let dogImage = `dog.${req.params.index}.jpg`;
-    if (!dogs.includes(dogImage)) res.status(404).json({"error":"index out or range"});
+    if (!dogs.includes(dogImage)) res.status(404).json({error: "index out or range", min: 1, max: dogs.length});
     res.sendFile(`${dir}/${dogImage}`);
 });
 
-app.get('/', function () {
-
+app.get('/', function (req, res) {
+    let dogImage = getRendomDog(dogs);
+    res.json({index: dogImage[0], image: `/dog/${dogImage[0]}`});
 });
 
 function getRendomDog(dogs) {
     let random = Math.floor(Math.random() * dogs.length);
-    return dogs[random];
+    return [random, dogs[random]];
 }
 
 module.exports = {
     start: function () {
-        app.listen(config.port, function () {
-            console.log(`Serving http://localhost:${config.port}/`);
+        app.listen(config.port, config.ip, function () {
+            console.log(`Serving http://${config.ip}:${config.port}/`);
         });
     }
 }
